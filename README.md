74bbfe0c0e7b66c6a6b95f663c2954188cba8aa2
j3rome <jerome.abdelnour@usherbrooke.ca>
2022-11-07 09:29:59 -0500

    First version of price-tracker

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
 # price-tracker
 Small webscraper to track price accross the web
+
+## Installation
+Install dependencies : 
+```
+pip install requests
+```
+
+## Configuration
+### Items
+Create a file named `items.json` at the root of the repo and add items you want to track. \
+(You can use `--items` argument to change the path) \
+Example : 
+```
+[
+  {
+    "name": "Dell 27 Inch 4K Monitor",
+    "url": "https://www.dell.com/en-ca/shop/dell-27-4k-uhd-monitor-s2721qs/apd/210-axlg/monitors-monitor-accessories",
+    "regex": "<div class=\"ps-dell-price ps-simplified\" data-testid=sharedPSPDellPrice>CAD \\$([0-9]+\\.?[0-9]*)</div>"
+  },
+  {
+    "name": "Item 2",
+    "url": "Url/to/product/page",
+    "regex": "pattern/to/capture/price"
+  },
+]
+```
+> The price must be captured in the first group of the `regex`.
+
+### Email notifications
+This script use [SendInBlue](https://www.sendinblue.com/) service to send email notifications. \
+First thing I found out of Google that had a reasonable free tier. \
+You simply need to create an account and generate an API key.
+
+
+You can then store this API key in `SEND_IN_BLUE_API_KEY` environment variable \
+or \
+`email.env` file at the root of the repo :
+```
+SEND_IN_BLUE_API_KEY={your_api_key}
+```
+
+## Usage
+```
+usage: Price tracker [-h] [-i ITEMS] [-e EMAIL]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -i ITEMS, --items ITEMS
+                        Path to items.json
+  -e EMAIL, --email EMAIL
+                        Email address at which to send notification
+```
+
+You can use `cron` jobs to run this script everyday.
+
+`Run price-tracker every day at 9:00` : 
+```
+# m h  dom mon dow   command
+0 9 * * * python /path/to/price-tracker.py -e your@email.com
+```
---
+++ price-tracker.py
@@ -0,0 +1,107 @@
+from datetime import datetime
+import argparse
+import json
+import os
+import pathlib
+import re
+import sys
+
+import requests
+
+script_path = pathlib.Path(__file__).parent.resolve()
+EMAIL_API_KEY = os.environ.get("SEND_IN_BLUE_API_KEY", None)
+
+parser = argparse.ArgumentParser("Price tracker")
+parser.add_argument("-i", "--items", type=str, default=script_path/"items.json", help="Path to items.json")
+parser.add_argument("-e", "--email", type=str, default=None, help="Email address at which to send notification")
+
+def log_error(msg: str) -> None:
+    print(f"[ERROR] {msg}", file=sys.stderr)
+
+def send_email(item_name: str, message: str, email: str) -> bool:
+    print("Sending email notification...")
+    if EMAIL_API_KEY is None:
+        log_error("Can't send email notification. No API key in environment variable SEND_IN_BLUE_API_KEY")
+        return False
+
+    r = requests.post("https://api.sendinblue.com/v3/smtp/email", 
+                       headers={
+                           "Accept": "application/json",
+                           "api-key": EMAIL_API_KEY,
+                           "Content-Type": "application/json"
+                       },
+                       json={
+                           "sender": {
+                               "name": "PriceWatcher",
+                               "email": "price@watch.com"
+                           },
+                           "to": [
+                               {
+                                   "email": email,
+                                   "name": "PriceWatcher Sub"
+                               }
+                           ],
+                           "subject": f"PriceWatcher ALERT for '{item_name}'",
+                           "htmlContent": message
+                       })
+    
+    if r.status_code != 201:
+        log_error(f"Could not send email for item '{item_name}'")
+        print(f"   {r.text}")
+        return False
+    
+    print("Email notification sent")
+    return True
+
+def main(args) -> None:
+    print(f"Loading items to track from '{args.items}'")
+
+    with open(args.items, 'r') as f:
+        items = json.load(f)
+    
+    if len(items) == 0:
+        log_error("Database must contain at least 1 item to track")
+        exit(1)
+    
+    for item in items:
+        print(f"Verifying price for '{item['name']}' from '{item['url']}'...")
+        r = requests.get(url=item['url'])
+        if r.status_code != 200:
+            log_error(f"Could not retrieve current price for item '{item['name']}'")
+            log_error(f"HTTP Request FAILED")
+            continue
+        
+        match = re.search(item['regex'], r.content.decode())
+        if match is None:
+            log_error(f"Could not retrieve current price for item '{item['name']}'")
+            log_error("Failed to carve out price from Http response")
+            continue
+        
+        current_price = float(match.group(1))
+        
+        if 'last_price' not in item or item['last_price'] > current_price:
+            msg = f"New BEST price found for '{item['name']}' : {current_price} $"
+            print(f">> {msg}")
+            if args.email is not None:
+                send_email(item['name'], msg, args.email)
+            item['last_price'] = current_price
+            item['last_price_timestamp'] = datetime.now().strftime("%s")
+
+    
+    print("All items have been verified. Updating database..")
+    with open(args.items, 'w') as f:
+        json.dump(items, f, indent=2)
+    print("All done")
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    if args.email is not None and EMAIL_API_KEY is None and os.path.exists(script_path / "email.env"):
+        with open(script_path / "email.env", 'r') as f:
+            EMAIL_API_KEY = f.read().strip().split("=")[-1]
+
+    if not os.path.exists(args.items):
+        log_error(f"Can't load item list from file '{args.items}'. It doesn't exist")
+        exit(1)
+
+    main(args)
+
